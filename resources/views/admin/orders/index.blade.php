2f1e6a80f39f7459913db692a9728a13599fa0e6
abdu rehman <arehmanewd@gmail.com>
2021-02-16 15:05:21 +0500

    updated

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -149,6 +149,70 @@
                         @if (isset($orders))
                             
                         @foreach ($orders as $order)
+                        @if ( $setting->order_status == 'all')
+                            <tr class="nk-tb-item">
+                            <td class="nk-tb-col nk-tb-col-check">
+                                <div class="custom-control custom-control-sm custom-checkbox notext">
+                                    <input type="checkbox" name="" class="order_check ">
+                                </div>
+                            </td>
+                            <td class="nk-tb-col">
+                                <div class="user-info">
+                                    <span class="tb-lead">{{ $order->id }}<span
+                                            class="dot dot-success d-md-none ml-1"></span></span>
+                                </div>
+                            </td>
+                            <td class="nk-tb-col">
+                                <div class="user-info">
+                                    <a href="{{ route('orders.show',$order->id) }}" >{{ $order->billing->first_name. " ".  $order->billing->last_name }}
+                                        </a>
+                                </div>
+                            </td>
+                            <td class="nk-tb-col tb-col-mb">
+                                <span class="tb-amount">{{ $order->status }}</span>
+                            </td>
+                            <td class="nk-tb-col tb-col-md">
+                                <span>{{$order->date_created}}</span>
+                            </td>
+                            <td class="nk-tb-col tb-col-lg" data-order="Email Verified - Kyc Unverified">
+                                {{ $order->total }}
+                            </td>
+                            <td class="nk-tb-col tb-col-lg">
+                            </td>
+                            <td class="nk-tb-col tb-col-lg">
+                                {{ count($order->line_items) }}
+                            </td>
+                            {{-- <td class="nk-tb-col tb-col-lg">
+                                <div class="form-group">
+                                    <div class="form-control-wrap">
+                                        <select class="form-select form-control form-control-lg" data-search="on">
+                                            <option value="default_option">Choose Curier service</option>
+                                            <option value="option_select_name">TCS</option>
+                                            <option value="option_select_name">Lepord</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </td> --}}
+                            <td class="nk-tb-col tb-col-md">
+                                {{-- <form action="{{ route('orders.show',$order->id) }}" method="post">
+                                <input type="hidden" name="store_url" class="store_url">
+                                <input type="hidden" name="consumer_key" class="consumer_key">
+                                <input type="hidden" name="consumer_secret" class="consumer_secret">
+                                <button type="submit" class="btn btn-sm btn-dim btn-primary"><i class="icon ni ni-eye"></i></button>
+                                </form> --}}
+                                <a href="{{ route('orders.show',$order->id) }}" class="btn btn-sm btn-dim btn-primary"><i class="icon ni ni-eye"></i></a>
+                                {{-- <button class="btn btn-sm btn-dim btn-primary order_status"
+                                    data-orderId="{{ $order->id }}"><i class="icon ni ni-pen"></i></button>
+                                    <button type="button" class="btn btn-sm btn-dim btn-primary orderNote" data-orderId="{{ $order->id }}"><i class="icon ni ni-plus"></i>Note</button>
+                                    <form action="{{ route('ordernotes.index') }}" method="get">
+                                    <input type="hidden" name="order_id" value="{{ $order->id }}">
+                                    <button type="subbmit" class="btn btn-sm btn-dim btn-primary" ><i class="icon ni ni-eye"></i> Note</button>
+                                    </form> --}}
+                            </td>
+
+                        </tr><!-- .nk-tb-item  -->
+                        @else
+                            
                         @if ($order->status == $setting->order_status)
 
                         <tr class="nk-tb-item">
@@ -212,6 +276,7 @@
                             </td>
 
                         </tr><!-- .nk-tb-item  -->
+                        @endif
                         @endif
                         @endforeach
                         @endif
